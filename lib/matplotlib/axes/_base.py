81211e199c1abf0e226e951aebbb037fd32a377a
Casey Brooks <casey.brooks@agyn.io>
2025-12-24 19:01:57 +0000

    fix(axes): reapply rc tick defaults on cla

--- lib/matplotlib/axes/_base.py
+++ lib/matplotlib/axes/_base.py
@@ -1309,6 +1309,44 @@ class _AxesBase(martist.Artist):
             self.yaxis.set_visible(yaxis_visible)
             self.patch.set_visible(patch_visible)
 
+        rcParams = mpl.rcParams
+        self.tick_params(
+            top=rcParams['xtick.top'] and rcParams['xtick.minor.top'],
+            bottom=rcParams['xtick.bottom'] and rcParams['xtick.minor.bottom'],
+            labeltop=(rcParams['xtick.labeltop'] and
+                      rcParams['xtick.minor.top']),
+            labelbottom=(rcParams['xtick.labelbottom'] and
+                         rcParams['xtick.minor.bottom']),
+            left=rcParams['ytick.left'] and rcParams['ytick.minor.left'],
+            right=rcParams['ytick.right'] and rcParams['ytick.minor.right'],
+            labelleft=(rcParams['ytick.labelleft'] and
+                       rcParams['ytick.minor.left']),
+            labelright=(rcParams['ytick.labelright'] and
+                        rcParams['ytick.minor.right']),
+            which='minor')
+
+        self.tick_params(
+            top=rcParams['xtick.top'] and rcParams['xtick.major.top'],
+            bottom=rcParams['xtick.bottom'] and rcParams['xtick.major.bottom'],
+            labeltop=(rcParams['xtick.labeltop'] and
+                      rcParams['xtick.major.top']),
+            labelbottom=(rcParams['xtick.labelbottom'] and
+                         rcParams['xtick.major.bottom']),
+            left=rcParams['ytick.left'] and rcParams['ytick.major.left'],
+            right=rcParams['ytick.right'] and rcParams['ytick.major.right'],
+            labelleft=(rcParams['ytick.labelleft'] and
+                       rcParams['ytick.major.left']),
+            labelright=(rcParams['ytick.labelright'] and
+                        rcParams['ytick.major.right']),
+            which='major')
+
+        label_outer_xaxis = getattr(self, "_label_outer_xaxis", None)
+        if label_outer_xaxis is not None and hasattr(self, "_subplotspec"):
+            label_outer_xaxis()
+        label_outer_yaxis = getattr(self, "_label_outer_yaxis", None)
+        if label_outer_yaxis is not None and hasattr(self, "_subplotspec"):
+            label_outer_yaxis()
+
         self.stale = True
 
     class ArtistList(MutableSequence):
